6eb6687a00b7913607c75e6b19560237f08ac9e1
Zelto Lira <you@example.com>
2023-02-02 05:43:42 -0300

    implementa Request e Response

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,31 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import java.time.LocalDate;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.petz.clientepet.cliente.domain.Sexo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
 public class ClienteRequest {
+
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNasciemento;
+	@CPF
+	private String cpf;
+	private Boolean aceitaTermos;
 
 }
