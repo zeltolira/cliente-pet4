916f19aec5024d498654711e937d894332cb4d12
Zelto Lira <you@example.com>
2023-02-05 09:49:40 -0300

    implementa clienteApplication e corrige clienteAPI

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
@@ -2,9 +2,12 @@ package br.com.petz.clientepet.cliente.application.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/v1/cliente")
@@ -12,5 +15,5 @@ public interface ClienteAPI {
 
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	ClienteResponse postCliente(ClienteRequest clienteRequest);
+	ClienteResponse postCliente(@Valid @RequestBody ClienteRequest clienteRequest);
 }
